d408ae840afa071ff769bc6b34260ccf555fecd4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-25 18:42:48 -0300

    refactor: implementa validação post /login com joi

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,36 +1,34 @@
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// const loginSchema = Joi.object({
-//   email: Joi.string().email().required().messages({
-//     'string.empty': '400|Some required fields are missing',
-//     'string.email': '400|Invalid fields',
-//     'any.required': '400|Some required fields are missing',
-//   }),
-//   password: Joi.string().min(2).required().messages({
-//     'string.empty': '400|Some required fields are missing',
-//     'string.min': '400|Invalid fields',
-//     'any.required': '400|Some required fields are missing',
-//   }),
-// });
+const loginSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.empty': '400|Some required fields are missing',
+    'any.required': '400|Some required fields are missing',
+  }),
+  password: Joi.string().required().messages({
+    'string.empty': '400|Some required fields are missing',
+    'any.required': '400|Some required fields are missing',
+  }),
+});
 
-// const validateLogin = (object) => {
-//   const { error } = loginSchema.validate(object);
-//   if (error !== undefined) {
-//     const [code, message] = error.message.split('|');
-//     return { code: Number(code), message };
-//   }
-//   return true;
-// };
-
-const validateLogin = ({ email, password }) => {
-  if (!email || email === '') {
-    return { code: 400, message: 'Some required fields are missing' };
-  }
-  if (!password || password === '') {
-    return { code: 400, message: 'Some required fields are missing' };
+const validateLogin = (object) => {
+  const { error } = loginSchema.validate(object);
+  if (error !== undefined) {
+    const [code, message] = error.message.split('|');
+    return { code: Number(code), message };
   }
   return true;
 };
+
+// const validateLogin = ({ email, password }) => {
+//   if (!email || email === '') {
+//     return { code: 400, message: 'Some required fields are missing' };
+//   }
+//   if (!password || password === '') {
+//     return { code: 400, message: 'Some required fields are missing' };
+//   }
+//   return true;
+// };
 
 module.exports = {
   validateLogin,
