21bc5e5b67fc825d8c1ac2bbefbf39f1c6ba085e
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-25 14:42:40 -0300

    Iniciando o Projeto :rocker:

--- src/api.js
+++ src/api.js
@@ -1,12 +1,8 @@
 const express = require('express');
 
-// ...
-
 const app = express();
 
 app.use(express.json());
-
-// ...
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
