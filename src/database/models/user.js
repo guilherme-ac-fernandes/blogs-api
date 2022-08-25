4559253095ded06e3f46aab96b17ac555d39d418
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-25 15:49:25 -0300

    feat: implementa model de user

---
+++ src/database/models/user.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: "id",
+      as: "blogPosts"
+    });
+  };
+
+  return User;
+};
