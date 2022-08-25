c74bd40656ce9d5776811a0a5dbb32565d43c27f
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-25 15:27:46 -0300

    feat: criar migration blogpost

---
+++ src/database/migrations/20220825180049-create-blog-posts.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("BlogPosts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable("BlogPosts");
+  }
+};
