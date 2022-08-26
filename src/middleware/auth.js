6352a128344412e518f61f8adbecbc609f89a42f
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-25 21:42:09 -0300

    comment: adiciona referencia do middleware de autentificação

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,6 +3,10 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
 
+// Middleware de autentificação proveniente da resolução do exercício
+// presente no course da trybe (referente ao dia 24.3)
+// Baseado na resolução do Aluno autor deste projeto (Guilherme Fernandes)
+// source: https://github.com/guilherme-ac-fernandes/trybe-exercicios/blob/main/03-back-end/bloco-24-node.js-orm-e-autentificacao/dia-04-testando-apis-com-testes-de-integracao/auth/validateJWT.js
 module.exports = async (req, res, next) => {
   try {
     const { authorization: token } = req.headers;
