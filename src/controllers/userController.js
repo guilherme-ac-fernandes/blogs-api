ba306b7cf4197f2efed97358a12f8814657174b4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-25 21:28:20 -0300

    fix: correção do retorno, após padroização

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,16 +7,16 @@ module.exports = {
     const { data, code, message } = await userService.login({
       email, password,
     });
-    if (code) return next({ code, message });
-    return res.status(code).json({ data });
+    if (message) return next({ code, message });
+    return res.status(code).json({ token: data });
   }),
   create: rescue(async (req, res, next) => {
     const { displayName, email, password, image } = req.body;
     const { data, code, message } = await userService.create({
       displayName, email, password, image,
     });
-    if (code) return next({ code, message });
-    return res.status(code).json({ data });
+    if (message) return next({ code, message });
+    return res.status(code).json({ token: data });
   }),
   getAll: rescue(async (_req, res, next) => {
     const { data, code, message } = await userService.getAll();
--- src/services/userService.js
+++ src/services/userService.js
@@ -6,11 +6,9 @@ module.exports = {
   login: async ({ email, password }) => {
     const validation = validateLogin({ email, password });
     if (validation.code) return validation;
-
     const user = await User.findOne({ where: { email } });
     if (!user) return { code: 400, message: 'Invalid fields' };
     if (user.dataValues.password !== password) return { code: 400, message: 'Invalid fields' };
-
     const token = createToken(email);
     return { code: 200, data: token };
   },
